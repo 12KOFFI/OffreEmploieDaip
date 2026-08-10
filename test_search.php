058d21d10883a38e5726b544879297257319f655
Isaac <isaacndri5@gmail.com>
2026-08-10 15:34:05 +0000

    chore: remove DataFixtures and test files

--- test_search.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$kernel = new App\Kernel('dev', true);
-$kernel->boot();
-$repo = $kernel->getContainer()->get('doctrine')->getRepository(App\Entity\Offre::class);
-dump(count($repo->rechercherOffresPubliees(['q' => 'Gimenez Bouvet S.A.S.'])));
